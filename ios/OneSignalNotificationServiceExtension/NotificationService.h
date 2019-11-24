08ef26fd68a6e0fed04432eca15605005743e381
Yash Atreya <yashatreya.ya@icloud.com>
2019-11-24 22:01:08 +0100

    OneSignal setup and tested ios + android

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,13 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Yash Atreya on 24/11/19.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
