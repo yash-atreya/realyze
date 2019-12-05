d0de572ab804edea0147eb4647cf34b6b952ac4e
Dharmi201 <dharmikumbhani201@gmail.com>
2019-12-05 21:23:27 +0530

    Added VectorIcons and Custom Fonts

--- app/screens/LogIn.js
+++ app/screens/LogIn.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {View, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
 
 class LogInScreen extends Component {
   constructor(props) {
@@ -9,7 +10,8 @@ class LogInScreen extends Component {
   render() {
     return (
       <View>
-        <Text>Login Page</Text>
+        <Text style={{fontFamily: 'Raleway-Bold'}}>Login Page</Text>
+        <Icon name="ios-people" color={'#000000'} size={24} />
       </View>
     );
   }
