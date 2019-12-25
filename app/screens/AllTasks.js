d7c158a0cffc4a66e5890a57314ca317b3a08d0e
Dharmi201 <dharmikumbhani201@gmail.com>
2019-12-25 22:43:20 +0530

    Added React Native Modal Library and New Task Modal

--- app/screens/AllTasks.js
+++ app/screens/AllTasks.js
@@ -1,5 +1,18 @@
 import React, {Component} from 'react';
-import {View, Text, TouchableOpacity, Platform, SafeAreaView} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Platform,
+  SafeAreaView,
+  Button,
+  TextInput,
+  FlatList,
+} from 'react-native';
+
+//Custom Components
+import PrimaryButton from '../components/PrimaryButton';
+import {styles, textInput, buttonStyles} from '../../styles';
 
 //3rd Party Libraries
 import {
@@ -9,11 +22,35 @@ import {
 import LinearGradient from 'react-native-linear-gradient';
 import Icon from 'react-native-vector-icons/Ionicons';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import {SlideAnimation, BottomModal} from 'react-native-modals';
 
 class AllTasksScreen extends Component {
   constructor(props) {
     super(props);
   }
+  state = {
+    visible: false,
+    text: '',
+  };
+  toggleModal = () => {
+    this.setState({visible: !this.state.visible});
+  };
+
+  passData() {
+    const text = this.state.text;
+    this.data.push({name: text}); 
+    this.setState({visible: false});
+  }
+
+  data = [{name: 'Yash'}, {name: 'Dharmi'}];
+
+  _renderItem = ({item}) => {
+    return (
+      <View>
+        <Text>{item.name}</Text>
+      </View>
+    );
+  };
 
   render() {
     return (
@@ -36,6 +73,59 @@ class AllTasksScreen extends Component {
           <Text style={{fontFamily: 'Raleway-Bold'}}>All Tasks</Text>
         </LinearGradient>
         <FontAwesome5 name="list-ul" color={'#000000'} size={24} />
+        <FlatList
+          data={this.data}
+          renderItem={this._renderItem}
+          keyExtractor={(item, index) => index.toString()}
+        />
+
+        <BottomModal
+          visible={this.state.visible}
+          onTouchOutside={() => {
+            this.setState({visible: false});
+          }}
+          modalAnimation={
+            new SlideAnimation({
+              slideFrom: 'bottom',
+            })
+          }
+          useNativeDriver={true}
+          width={wp('100%')}
+          height={hp('91.2%')}
+          modalStyle={{
+            borderTopLeftRadius: 20,
+            borderTopRightRadius: 20,
+            backgroundColor: '#D5D7DB',
+          }}
+          hasBackdrop={true}
+          hideModalContentWhileAnimating={true}>
+          <View>
+            <Text>Hello</Text>
+          </View>
+          <TextInput
+            placeholder="Hello"
+            onChangeText={newtext => this.setState({text: newtext})}
+          />
+          {/* {console.log(this.state.text)} */}
+          <TouchableOpacity
+            onPress={() => this.passData()}
+            style={{
+              shadowColor: '#102FC6',
+              shadowOpacity: 0.3,
+              shadowOffset: {width: 0, height: 7},
+              shadowRadius: 11,
+            }}>
+            <LinearGradient
+              start={{x: 0, y: 0}}
+              end={{x: 1, y: 0}}
+              colors={['#00A1ED', '#0A3BC6']}
+              style={buttonStyles.buttonBody}>
+              <Text style={[styles.h1PBW, {fontSize: 20}]}>
+                Create New Task
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </BottomModal>
       </View>
     );
   }
